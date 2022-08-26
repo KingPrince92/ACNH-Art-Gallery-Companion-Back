f162e1487785853d16569b683b5cd0809273b28c
SimonOrow <105259087+SimonOrow@users.noreply.github.com>
2022-08-26 16:13:36 -0400

    backend

--- functions/src/routes/guestbookRouter.ts
+++ functions/src/routes/guestbookRouter.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { getClient } from "../db";
-import GuestbookComment from "../models/GuestbookComment";
+import ArtCollection from "../models/ArtCollection";
 
 const guestbookRouter = express.Router();
 
@@ -11,16 +11,50 @@ const errorResponse = (error: any, res: any) => {
 
 guestbookRouter.get("/:uid", async (req, res) => {
   try {
+    const uid: string | undefined = req.params.uid;
     const client = await getClient();
-    const results = await client
+    const query: any = { uid };
+    const cursor = client
       .db()
-      .collection<GuestbookComment>("guestbook")
-      .find()
-      .toArray();
+      .collection<ArtCollection>("guestbook")
+      .find(query);
+    const results = await cursor.toArray();
+    res.status(200);
     res.json(results);
   } catch (err) {
     errorResponse(err, res);
   }
 });
 
+guestbookRouter.post("/", async (req, res) => {
+  try {
+    const client = await getClient();
+    const newArt: ArtCollection = req.body;
+    await client.db().collection<ArtCollection>("guestbook").insertOne(newArt);
+    res.status(200);
+    res.json(newArt);
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
+guestbookRouter.delete("/:name/user/:uid", async (req, res) => {
+  try {
+    const name: string = req.params.name;
+    const uid: string = req.params.uid;
+    const client = await getClient();
+    const result = await client
+      .db()
+      .collection<ArtCollection>("guestbook")
+      .deleteOne({ name, uid });
+    if (result.deletedCount) {
+      res.sendStatus(204);
+    } else {
+      res.status(404);
+      res.send("Not Found");
+    }
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
 export default guestbookRouter;
