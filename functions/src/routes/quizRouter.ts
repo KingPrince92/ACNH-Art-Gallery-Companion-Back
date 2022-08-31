798d610f2f7e742acc340120791c5f76b8d0f863
SimonOrow <105259087+SimonOrow@users.noreply.github.com>
2022-08-31 14:42:44 -0400

    Leaderboard

---
+++ functions/src/routes/quizRouter.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import { getClient } from "../db";
+import QuizCollection from "../models/QuizCollection";
+
+const quizRouter = express.Router();
+
+const errorResponse = (error: any, res: any) => {
+  console.error("FAIL", error);
+  res.status(500).json({ message: "Internal Server Error" });
+};
+
+quizRouter.get("/", async (req, res) => {
+  try {
+    const client = await getClient();
+    const cursor = client
+      .db()
+      .collection<QuizCollection>("quizscores")
+      .find()
+      .sort(-1)
+      .limit(10);
+    const results = await cursor.toArray();
+    res.status(200);
+    res.json(results);
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
+quizRouter.post("/", async (req, res) => {
+  try {
+    const client = await getClient();
+    const newScore: QuizCollection = req.body;
+    await client
+      .db()
+      .collection<QuizCollection>("quizscores")
+      .insertOne(newScore);
+    res.status(200);
+    res.json(newScore);
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
