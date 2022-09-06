d62e39f9a9e87c1108a71f9e58bede80e2eb9f0f
Milo <miloprince92@gmail.com>
2022-09-06 11:21:41 -0400

    refactor

--- functions/src/models/ArtCollection.ts
+++ functions/src/models/ArtCollection.ts
@@ -1,7 +1,15 @@
-import { ObjectId } from "mongodb";
-
 export default interface ArtCollection {
-  _id?: ObjectId;
-  image_url: string;
   name: string;
+  url: string;
+  image_url: string;
+  has_fake: boolean;
+  fake_image_url: string;
+  art_name: string;
+  author: string;
+  year: string;
+  art_style: string;
+  description: string;
+  buy: number;
+  sell: number;
+  authenticity: string;
 }
